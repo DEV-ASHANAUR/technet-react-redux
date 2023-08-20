33a4237c4457445bb34f065b7367ba520c142c64
dev-ashanaur <devashanaur009@gmail.com>
2023-08-20 19:42:51 +0600

    [DONE] add to cart,product filter

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -13,14 +13,13 @@ import {
 } from 'react-icons/hi';
 import { Button } from './ui/button';
 import { IProduct } from '@/types/globalTypes';
+import { useAppDispatch, useAppSelector } from '@/redux/hooks';
+import { addToCart, removeFromCart, removeOne } from '@/redux/features/cart/CartSlice';
 
 export default function Cart() {
-  //! Dummy data
+  const {products,total} = useAppSelector((state) => state.cart);
 
-  const products: IProduct[] = [];
-  const total = 0;
-
-  //! **
+  const dispatch = useAppDispatch();
 
   return (
     <Sheet>
@@ -52,13 +51,14 @@ export default function Cart() {
                 </p>
               </div>
               <div className="border-l pl-5 flex flex-col justify-between">
-                <Button>
+                <Button onClick={()=>dispatch(addToCart(product))}>
                   <HiOutlinePlus size="20" />
                 </Button>
-                <Button>
+                <Button onClick={()=>dispatch(removeOne(product))}>
                   <HiMinus size="20" />
                 </Button>
                 <Button
+                  onClick={()=> dispatch(removeFromCart(product))}
                   variant="destructive"
                   className="bg-red-500 hover:bg-red-400"
                 >
---
+++ src/redux/store.ts
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './features/cart/CartSlice';
+import productReducer from './features/products/productSlice';
+
+const store = configureStore({
+  reducer: { cart: cartReducer,product:productReducer },
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
